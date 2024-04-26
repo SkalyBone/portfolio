7dff9377e420e02d15ef54f72a07f29bea39150b
SkalyBone <alejustogar3@gmail.com>
2024-04-26 14:53:25 -0600

    Primer commit

---
+++ js/style-switcher.js
@@ -0,0 +1,48 @@
+/*====== TOGGLER STYLE SWITCHER ======*/
+
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+    console.log("Hola");
+})
+
+//hide styñe switcher on scroll
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open")){
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+
+// THEME COLOR ==============
+const alterateStyles = document.querySelectorAll(".alternate-style");
+function SetActiveStyle(color)
+{
+    alterateStyles.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
+
+// MODO OSCURO ===================================
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
